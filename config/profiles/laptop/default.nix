cbe9ebdc5fbdec41787641daf5abe82476b16ba2
Moritz Lutz <moritz.lutz@gmail.com>
2021-10-03 16:18:39 +0200

    Update default.nix
    pipewire

--- config/profiles/laptop/default.nix
+++ config/profiles/laptop/default.nix
@@ -39,12 +39,52 @@
         };
     };
     
-
-#    sound.enable = true;
-    hardware = {
-      pulseaudio.enable = true;
-      bluetooth.enable = true;
+    security.rtkit.enable = true;
+    services.bluetooth.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        config.pipewire = {
+            "context.properties" = {
+                #"link.max-buffers" = 64;
+                "link.max-buffers" = 16; # version < 3 clients can't handle more than this
+                "log.level" = 2; # https://docs.pipewire.org/#Logging
+                #"default.clock.rate" = 48000;
+                #"default.clock.quantum" = 1024;
+                #"default.clock.min-quantum" = 32;
+                #"default.clock.max-quantum" = 8192;
+            };
+        };
+        media-session.config.bluez-monitor.rules = [
+            {
+                # Matches all cards
+                matches = [ { "device.name" = "~bluez_card.*"; } ];
+                actions = {
+                    "update-props" = {
+                        "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+                        # mSBC is not expected to work on all headset + adapter combinations.
+                        "bluez5.msbc-support" = true;
+                        # SBC-XQ is not expected to work on all headset + adapter combinations.
+                        "bluez5.sbc-xq-support" = true;
+                    };
+                };
+            }
+            {
+                matches = [
+                    # Matches all sources
+                    { "node.name" = "~bluez_input.*"; }
+                    # Matches all outputs
+                    { "node.name" = "~bluez_output.*"; }
+                ];
+                actions = {
+                    "node.pause-on-idle" = false;
+               };
+            }
+        ];
     };
+    
     users.extraUsers.espo.extraGroups = [ "audio" ];
 
      # Install fonts
