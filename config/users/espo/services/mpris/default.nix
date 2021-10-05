5f7877e427cf9030b68534607dd63f144477c4a3
Moritz Lutz <moritz.lutz@gmail.com>
2021-10-05 11:22:41 +0200

    Create default.nix

---
+++ config/users/espo/services/mpris/default.nix
@@ -0,0 +1,6 @@
+systemd.user.services.mpris-proxy = {
+  Unit.Description = "Mpris proxy";
+  Unit.After = [ "network.target" "sound.target" ];
+  Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  Install.WantedBy = [ "default.target" ];
+};
