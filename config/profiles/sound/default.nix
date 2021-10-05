2d11cb8a947658077caf7f1628bb9cf1d364b101
Moritz Lutz <moritz.lutz@gmail.com>
2021-10-05 11:08:10 +0200

    Update default.nix
    changes to pipewire

--- config/profiles/sound/default.nix
+++ config/profiles/sound/default.nix
@@ -11,6 +11,73 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
+        config.pipewire = {
+          "context.properties" = {
+            "link.max-buffers" = 16;
+            "log.level" = 2;
+            "default.clock.rate" = 48000;
+            "default.clock.quantum" = 32;
+            "default.clock.min-quantum" = 32;
+            "default.clock.max-quantum" = 32;
+            "core.daemon" = true;
+            "core.name" = "pipewire-0";
+          };
+          "context.modules" = [ {
+            name = "libpipewire-module-rtkit";
+            args = {
+              "nice.level" = -15;
+              "rt.prio" = 88;
+              "rt.time.soft" = 200000;
+              "rt.time.hard" = 200000;
+            };
+            flags = [ "ifexists" "nofail" ];
+          }
+          { name = "libpipewire-module-protocol-native"; }
+          { name = "libpipewire-module-profiler"; }
+          { name = "libpipewire-module-metadata"; }
+          { name = "libpipewire-module-spa-device-factory"; }
+          { name = "libpipewire-module-spa-node-factory"; }
+          { name = "libpipewire-module-client-node"; }
+          { name = "libpipewire-module-client-device"; }
+          {
+            name = "libpipewire-module-portal";
+            flags = [ "ifexists" "nofail" ];
+          }
+          {
+            name = "libpipewire-module-access";
+            args = {};
+          }
+          { name = "libpipewire-module-adapter"; }
+          { name = "libpipewire-module-link-factory"; }
+          { name = "libpipewire-module-session-manager"; }
+          { name = libpipewire-module-filter-chain
+            args = {
+              node.name =  "rnnoise_source"
+              node.description =  "Noise Canceling source"
+              media.name =  "Noise Canceling source"
+              filter.graph = {
+                  nodes = [
+                      {
+                          type = ladspa
+                          name = rnnoise
+                          plugin = ladspa/librnnoise_ladspa
+                          label = noise_suppressor_stereo
+                          control = {
+                            "VAD Threshold (%)" 50.0
+                          }
+                      }
+                  ]
+              }
+              capture.props = {
+                node.passive = true
+              }
+              playback.props = {
+                media.class = Audio/Source
+              }
+            }
+          }];
+        };
+        #bluetooth stuff
         media-session.config.bluez-monitor.rules = [
             {
                 # Matches all cards
@@ -18,10 +85,8 @@
                 actions = {
                     "update-props" = {
                         "bluez5.auto-connect" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-                        # mSBC is not expected to work on all headset + adapter>
-                        #"bluez5.msbc-support" = true;
-                        # SBC-XQ is not expected to work on all headset + adapt>
-                        #"bluez5.sbc-xq-support" = true;
+                        "bluez5.msbc-support" = true;
+                        "bluez5.sbc-xq-support" = true;
                     };
                 };
             }
