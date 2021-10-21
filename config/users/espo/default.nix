dae92f0c12631944bd62c2937bbb6d5405704977
Moritz Lutz <moritz.lutz@gmail.com>
2021-10-21 07:23:05 +0200

    Update default.nix

--- config/users/espo/default.nix
+++ config/users/espo/default.nix
@@ -6,10 +6,7 @@
     home.sessionVariables = {
       MOZ_ENABLE_WAYLAND = "1";
       MOZ_USE_XINPUT2 = "1";
-
-      #WLR_DRM_NO_MODIFIERS = "1";
       SDL_VIDEODRIVER = "wayland";
-      # needs qt5.qtwayland in systemPackages
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       _JAVA_AWT_WM_NONREPARENTING = "1";
