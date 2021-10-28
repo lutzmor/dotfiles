0ea4eb68c4323697e517494803c01f0747881602
Moritz Lutz <moritz.lutz@gmail.com>
2021-10-28 09:19:45 +0200

    Update default.nix

--- config/hosts/bambam/default.nix
+++ config/hosts/bambam/default.nix
@@ -24,7 +24,7 @@
      luks = {
        fido2Support = true;
        devices."enc" = {
-         device = "/dev/nvme0n1p2";
+         device = "/dev/nvme0n1p3";
          preLVM = true;
 	 #fido2.credential = "1a3742467558aa5e00dc58197d6e4ee7be4431f0a689c5000aad6c21d94f798b418852dae2262619c1da2be7562ec9dd94888c71a9326fea70dfe16214b5ea8ec0143d030000";
          #fido2.passwordLess = true;
@@ -33,22 +33,36 @@
    };
   };
   
-  fileSystems."/" =
-    { device = "/dev/mapper/enc";
-      fsType = "btrfs";
-      options = [ "subvol=nixos" "compress=zstd" "autodefrag" "noatime"];
-    };
-
   fileSystems."/home" =
     { device = "/dev/mapper/enc";
       fsType = "btrfs";
-      options = [ "subvol=home" "compress=zstd" "autodefrag" "noatime"];
+      options = [ "subvol=home" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/mapper/enc";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+  fileSystems."/persist" =
+    { device = "/dev/mapper/enc";
+      fsType = "btrfs";
+      options = [ "subvol=persist" ];
+    };
+
+  fileSystems."/var/log" =
+    { device = "/dev/mapper/enc";
+      fsType = "btrfs";
+      options = [ "subvol=log" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-label/nixos-boot";
+    { device = "/dev/nvme0n1p1";
       fsType = "vfat";
     };
 
-  #swapDevices = [ { device = "/dev/nvme0n1p2"; } ];
+  swapDevices =
+    [ { device = "/dev/nvme0n1p2"; }
+    ];
 }
